2bfbbc9da4df18ed46d75cf168b132543578c320
NarDd <1601037@SIT.Singaporetech.edu.sg>
2017-11-29 15:21:31 +0800

    Added date time picker

--- app/src/main/java/com/kkontagion/sugarbaby/MealCreatorActivity.java
+++ app/src/main/java/com/kkontagion/sugarbaby/MealCreatorActivity.java
@@ -1,11 +1,18 @@
 package com.kkontagion.sugarbaby;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.kkontagion.sugarbaby.adapters.AutocompleteAdapter;
 import com.kkontagion.sugarbaby.adapters.FoodAdapter;
@@ -30,6 +37,10 @@ public class MealCreatorActivity extends AppCompatActivity {
     private Calendar time;
     private ArrayList<Food> autoarray;
     private static SimpleDateFormat df = new SimpleDateFormat("dd MMM yyyy hh:mmaa");
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener;
+
+    int day,month,year,hour,minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +62,7 @@ public class MealCreatorActivity extends AppCompatActivity {
         completeAdapter = new AutocompleteAdapter(this, R.layout.item_autocomplete, autoarray);
         tvComplete.setAdapter(completeAdapter);
         tvComplete.setThreshold(1);
+
         tvComplete.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
 
         tvTime.setText(df.format(time.getTime()));
@@ -72,7 +84,50 @@ public class MealCreatorActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // TODO datepicker dialog, followed by time picker dialog.
                 // On complete (both selected), update tvTime.setText(result) using the same format as `df` above.
+                // Result should update Calendar time variable
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MealCreatorActivity.this,
+                        R.style.TimePickerTheme,
+                        mDateSetListener,
+                        time.get(Calendar.YEAR),time.get(Calendar.MONTH),time.get(Calendar.DAY_OF_MONTH));
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
             }
         });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1,int i2){
+                year = i;
+                month = i1;
+                day = i2;
+
+                time.set(i, i1, i2);
+
+                hour = time.get(Calendar.HOUR_OF_DAY);
+                minute = time.get(Calendar.MINUTE);
+
+                TimePickerDialog dialog = new TimePickerDialog(MealCreatorActivity.this,mTimeSetListener,hour,minute,false);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+
+
+        };
+
+        mTimeSetListener = new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i , int i1){
+                hour = i;
+                minute = i1;
+                time.set(Calendar.HOUR_OF_DAY,i);
+                time.set(Calendar.MINUTE,i1);
+//                Log.d("penis", "onTimeSet: " + time.toString());
+                tvTime.setText(df.format(time.getTime()));
+            }
+        };
+
+
+
     }
 }
